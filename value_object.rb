6ad7055379d3c83b82b9b86a59a5ec50d98e930d
trikitrok <trikitrok@gmail.com>
2015-08-26 15:55:48 +0200

    Value objects with accessors, equality and invariants

---
+++ value_object.rb
@@ -0,0 +1,64 @@
+class NotImplementedInvariant < Exception
+  def initialize(name)
+    super "The invariant #{name} is not implemented"
+  end
+end
+
+class ViolatedInvariant < Exception
+  def initialize(name, wrong_values)
+    super "Fields values " + wrong_values.to_s + " violate invariant #{name}"
+  end
+end
+
+module ValueObject
+  BUILT_IN_INVARIANTS = {
+    :integers => Proc.new do |obj|
+      obj.values.all? {|value| value.is_a? Integer}
+    end
+  }
+
+  def fields(*names)
+    attr_reader(*names)
+
+    define_method(:initialize) do |*values|
+      names.zip(values) do |name, value|
+        instance_variable_set(:"@#{name}", value)
+      end
+      check_invariant
+    end
+
+    define_method(:values) do
+      names.map { |field| send(field) }
+    end
+
+    define_method(:to_a) do
+      values
+    end
+
+    define_method(:eql?) do |other|
+      self.class == other.class && values == other.values
+    end
+
+    define_method(:==) do |other|
+      eql?(other)
+    end
+  end
+
+  def invariants(*predicates)
+    define_method(:check_invariant) do
+      predicates.each do |predicate|
+        begin
+          res = send(predicate)
+        rescue
+          proc = BUILT_IN_INVARIANTS[predicate]
+
+          raise NotImplementedInvariant.new(predicate) unless proc
+
+          res = proc.call(self) if proc
+        end
+
+        raise ViolatedInvariant.new(predicate, self.values) unless res
+      end
+    end
+  end
+end
--- value_object_spec.rb
+++ value_object_spec.rb
@@ -1,6 +1,61 @@
+require "./value_object"
+
 describe "a value object" do
 
-  it "fix me" do
-    expect(true).to be false
+  class Point
+    extend ValueObject
+    fields :x, :y
+    invariants :inside_first_cuadrant, :integers
+
+    private
+    def inside_first_cuadrant
+      x > 0 && y > 0
+    end
+  end
+
+  it "declaratively generates a constructor, fields and accessors" do
+    a_value_object = Point.new(5, 3)
+
+    expect(a_value_object.x).to eq(5)
+    expect(a_value_object.y).to eq(3)
+  end
+
+  it "provides value object equality" do
+    a_value_object = Point.new(5, 3)
+    same_value_object = Point.new(5, 3)
+    different_value_object = Point.new(6, 3)
+
+    expect(a_value_object).to eq(same_value_object)
+    expect(a_value_object).to_not eq(different_value_object)
+  end
+
+  it "provides a to_a method" do
+    a_value_object = Point.new(5, 3)
+
+    expect(a_value_object.to_a).to eq([5, 3])
+  end
+
+  it "forces invariants declarated on all fields" do
+    expect{Point.new(-5, 3)}.to raise_error(
+      ViolatedInvariant, "Fields values [-5, 3] violate invariant inside_first_cuadrant"
+    )
+  end
+
+  it "provides a built-in invariant for integer values" do
+    expect{Point.new(5.2, 3)}.to raise_error(
+      ViolatedInvariant, "Fields values [5.2, 3] violate invariant integers"
+    )
+  end
+
+  it "raises an exception when an invariant has not been implemented" do
+    class Dummy
+      extend ValueObject
+      fields :x
+      invariants :not_implemented
+    end
+
+    expect{Dummy.new(5)}.to raise_error(
+      NotImplementedInvariant, "The invariant not_implemented is not implemented"
+    )
   end
 end
